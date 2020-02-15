68972ce6361ddf356c51c5b31ed0bc67a6b68215
YajatMittal <59126036+YajatMittal@users.noreply.github.com>
2020-02-15 18:19:36 -0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,78 +1,56 @@
-#This program has been created by Yajat Mittal
-#The program has been created on 20th Sep 2019
-#This has been created for learning and fun
-import sys
+#This is a program which allows you too do the following operations
 print("******************************")
 print("*    MATHS CALCULATIONS      *")
 print("******************************")
 print("* 1. Addition                *")
-print("*                            *")
 print("* 2. Subtraction             *")
-print("*                            *")
 print("* 3. Multiplication          *")
-print("*                            *")
 print("* 4. Division                *")
-print("*                            *")
-print("* 5. Exit Program            *")
-print("*                            *")
 print("* Press option for operation *")
-print("*                            *")
 print("******************************")
 
-print("Enter the Option ---->")
-option = int(sys.stdin.readline())
+#Enter your option
+option = int(input("Enter the Option ---->"))
 
-#print("You have entered option # %s" %option)
 
 if option ==1:
-  print("You want to do addition...")
+    print("You want to do addition...")
   
-  print("Enter the first number -->")
-  firstnum = int(sys.stdin.readline())
+    firstnum = int(input("Enter the first number -->"))
   
-  print("Enter the second number -->")
-  secondnum = int(sys.stdin.readline())
-  resultAddition = firstnum + secondnum
+    secondnum = int(input("Enter the second number -->"))
+    resultAddition = firstnum + secondnum
 
-  print("The result of addition of %s and %s is = %s"% (firstnum,secondnum,resultAddition))
+    print("The result of addition of %s and %s is = %s"% (firstnum,secondnum,resultAddition))
 
 elif option ==2:
-  print ("You want to do subtraction")
+    print ("You want to do subtraction")
 
-  print("Enter the first number-->")
-  firstnum=int(sys.stdin.readline())
+    firstnum = int(input("Enter the first number -->"))
 
-  print("Enter the second number-->")
-  secondnum=int(sys.stdin.readline())
+    secondnum = int(input("Enter the second number -->"))
 
-  finalresult=firstnum - secondnum
+    finalresult = firstnum - secondnum
 
-  print("The difference between the two numbers is = %s"% (finalresult))
+    print("The difference between the two numbers is = %s"% (finalresult))
 
 elif option ==3:
-  print("You want to do multiplication")
+    print("You want to do multiplication")
 
-  print("Enter the  first number-->")
-  firstnum=int(sys.stdin.readline())
+    firstnum = int(input("Enter the first number -->"))
 
-  print("Enter the  second number-->")
-  secondnum=int(sys.stdin.readline())
+    secondnum = int(input("Enter the second number -->"))
 
-  final = firstnum * secondnum
-  print("The product of the two numbers is = %s"% (final))
+    final = firstnum * secondnum
+    print("The product of the two numbers is = %s"% (final))
 
 elif option ==4:
-  print("You want to do division")
+    print("You want to do division")
 
-  print("Enter the first number-->")
-  firstnum=int(sys.stdin.readline())
+    firstnum = int(input("Enter the first number-->"))
 
-  print("Enter the second number-->")
-  secondnum=int(sys.stdin.readline())
+    secondnum = int(input("Enter the second number -->"))
 
-  quotient=firstnum / secondnum
-  print("The quotient is %s"%(quotient))
+    quotient = firstnum / secondnum
+    print("The quotient is %s"%(quotient))
 
-
-else: 
-  print("Exiting.....")
